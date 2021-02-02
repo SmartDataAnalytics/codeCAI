08b5e2d230207cff7e60480ad792d1a154f9c555
Bernhard Stadler <stadlerb@users.noreply.github.com>
2021-02-02 16:02:58 +0100

    Corrected license

--- scads_jupyter_nli/setup.py
+++ scads_jupyter_nli/setup.py
@@ -77,7 +77,6 @@ setup_args = dict(
     platforms="Linux, Mac OS X, Windows",
     keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
     classifiers=[
-        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
