a74a102193714b6cc49ea7c20f0277510bb627fd
Bernhard Stadler <stadlerb@users.noreply.github.com>
2021-02-02 18:35:33 +0100

    'Show ScaDS NLI' command in palette

--- scads_jupyter_nli/src/index.ts
+++ scads_jupyter_nli/src/index.ts
@@ -2,6 +2,10 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
+
+
 
 /**
  * Initialization data for the scads_jupyter_nli extension.
@@ -9,8 +13,28 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'scads_jupyter_nli:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension scads_jupyter_nli is activated!');
+
+    const content = new Widget()
+    const widget = new MainAreaWidget({ content })
+
+    widget.id = 'scads-jupyter-nli-panel'
+    widget.title.label = 'ScaDS NLI'
+    widget.title.closable = true
+
+    const command = 'sjn:open'
+    app.commands.addCommand(command, {
+      label: 'Show ScaDS NLI',
+      execute: () => {
+        if (!widget.isAttached) {
+          app.shell.add(widget, 'main')
+        }
+      }
+    })
+
+    palette.addItem({ command, category: 'NLI' })
   }
 };
 
