9d2f29fc47c33c3ae10a1e3c08c7512d3bfd8ef4
Klaudia <KlaudiaTH@users.noreply.github.com>
2021-02-20 09:54:24 +0100

    Handling empty results from beam search.

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -16,53 +16,58 @@ from scads_cai_prototype.transformerinf import Transformer
 
 class ActionNl2Code(Action):
 
-     def __init__(self):
-         nl2code_conf = os.getenv('NL2CODE_CONF')
-         nl2code_yaml = open(nl2code_conf)
-         parsed_nl2code_yaml = yaml.load(nl2code_yaml, Loader=yaml.FullLoader)
+    def __init__(self):
+        nl2code_conf = os.getenv('NL2CODE_CONF')
+        nl2code_yaml = open(nl2code_conf)
+        parsed_nl2code_yaml = yaml.load(nl2code_yaml, Loader=yaml.FullLoader)
 
-         self.preprocinf = Preprocinf(parsed_nl2code_yaml["preproc"]["vocabsrc-file"])
+        self.preprocinf = Preprocinf(parsed_nl2code_yaml["preproc"]["vocabsrc-file"])
 
-         model = Transformer.load_from_checkpoint(parsed_nl2code_yaml["model_conf"]["test-model-path"])
-         grammargraph = GrammarGraphLoader(parsed_nl2code_yaml["model_conf"]["grammar-graph-file"]).load_graph()
-         codegenerator = PythonCodeGenerator(grammargraph)
+        model = Transformer.load_from_checkpoint(parsed_nl2code_yaml["model_conf"]["test-model-path"])
+        grammargraph = GrammarGraphLoader(parsed_nl2code_yaml["model_conf"]["grammar-graph-file"]).load_graph()
+        codegenerator = PythonCodeGenerator(grammargraph)
 
-         self.beamsearch = BeamSearch(
-             model=model,
-             grammargraph=grammargraph,
-             codegenerator=codegenerator,
-             num_beams=parsed_nl2code_yaml["model_conf"]["num-beams"],
-             disable_constraint_mask=parsed_nl2code_yaml["model_conf"]["disable-constraint-mask"],
-             max_beam_length=parsed_nl2code_yaml["model_conf"]["max-beam-length"],
-             max_num_predicted_results=parsed_nl2code_yaml["model_conf"]["max-num-predicted-results"],
-             mode=parsed_nl2code_yaml["model_conf"]["beam-search-mode"],
-             keep_invalid_results=parsed_nl2code_yaml["model_conf"]["keep-invalid-beamsearch-results"])
+        self.beamsearch = BeamSearch(
+            model=model,
+            grammargraph=grammargraph,
+            codegenerator=codegenerator,
+            num_beams=parsed_nl2code_yaml["model_conf"]["num-beams"],
+            disable_constraint_mask=parsed_nl2code_yaml["model_conf"]["disable-constraint-mask"],
+            max_beam_length=parsed_nl2code_yaml["model_conf"]["max-beam-length"],
+            max_num_predicted_results=parsed_nl2code_yaml["model_conf"]["max-num-predicted-results"],
+            mode=parsed_nl2code_yaml["model_conf"]["beam-search-mode"],
+            keep_invalid_results=parsed_nl2code_yaml["model_conf"]["keep-invalid-beamsearch-results"])
 
-         print('action nl2code')
+        print('action nl2code')
 
-     def name(self) -> Text:
-         return "action_nl2code"
+    def name(self) -> Text:
+        return "action_nl2code"
 
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-         nl_input = tracker.latest_message.get('text')
-         print('message',nl_input)
+        nl_input = tracker.latest_message.get('text')
+        print('message', nl_input)
 
-         nl_seq = self.preprocinf.preproc(nl_input)
-         print('nl_seq', nl_seq)
+        nl_seq = self.preprocinf.preproc(nl_input)
+        print('nl_seq', nl_seq)
 
-         nl_seq = torch.tensor(nl_seq)
-         nl_seq = nl_seq.view(-1, 1)
+        nl_seq = torch.tensor(nl_seq)
+        nl_seq = nl_seq.view(-1, 1)
 
-         prediction = self.beamsearch.perform_beam_search(nl_seq, None)
-         _, _, code_snippet = prediction[0]
-         print('code_snippet', code_snippet)
+        prediction = self.beamsearch.perform_beam_search(nl_seq, None)
 
-         dispatcher.utter_message(json_message={"nl_input": nl_input, "code_snippet": code_snippet})
+        if prediction:
+            _, _, code_snippet = prediction[0]
+            print('code_snippet', code_snippet)
+            dispatcher.utter_message(json_message={"nl_input": nl_input, "code_snippet": code_snippet})
+        else:
+            response = "Could not translate to code. Please try reformulating your question."
+            print(response)
+            dispatcher.utter_message(text=response)
 
-         return []
+        return []
 
 
 class ActionOrganizeKMeansImports(Action):
@@ -76,12 +81,13 @@ class ActionOrganizeKMeansImports(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        imports = "from sklearn.datasets import make_blobs\n" \
+                  "import pandas as pd\n" \
+                  "import numpy as np\n" \
+                  "import matplotlib.pyplot as plt\n" \
+                  "from sklearn.cluster import KMeans\n"
 
-        dispatcher.utter_message(text="from sklearn.datasets import make_blobs\n"
-                                      "import pandas as pd\n"
-                                      "import numpy as np\n"
-                                      "import matplotlib.pyplot as plt\n"
-                                      "from sklearn.cluster import KMeans\n")
+        dispatcher.utter_message(json_message={"nl_input": "Organize kmeans imports", "code_snippet": imports})
 
         return []
 
@@ -96,9 +102,12 @@ class ActionOrganizeKMeansImports(Action):
         def run(self, dispatcher: CollectingDispatcher,
                 tracker: Tracker,
                 domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            dispatcher.utter_message(text="from sklearn.datasets import load_iris\n"
-                                          "from sklearn.tree import DecisionTreeClassifier\n"
-                                          "from sklearn.tree import export_text")
+            imports = "from sklearn.datasets import load_iris\n" \
+                      "from sklearn.tree import DecisionTreeClassifier\n" \
+                      "from sklearn.tree import export_text"
+
+            dispatcher.utter_message(
+                json_message={"nl_input": "Organize decision tree imports", "code_snippet": imports})
 
             return []
 
